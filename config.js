e2c4af489f7fe2b35b70de1da95a54e66cb49adb
LoyceLow <73449944+LoyceLow@users.noreply.github.com>
2020-10-26 09:24:13 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ var config = {
         "David",  // 同上...
         "Happy Birthday!",
         "May your birthday",
-        "be the start of a year filled with",
-        "good luck",
-        "good health and",
-        "much happiness!",
+        "be the start of a year",
+        "filled with",
+        "good luck,",
+        "good health",
+        "and much happiness!",
         "祝你生日快乐",
         "心想事成",
-        "愿望成真",
     ],
     /**
      * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
@@ -25,14 +25,14 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "心爱的小可爱": "./imgs/xiaokeai.png",
+        // "心爱的小可爱": "./imgs/xiaokeai.png",
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "Start",
+        turn_on: "Press to Start",
         play: "Music, please~",
-        bannar_coming: "Color",
+        bannar_coming: "Balloon",
         balloons_flying: "好像少点东西",
         cake_fadein: "Cake？",
         light_candle: "Candle？",
